65b24a8968bb5c89fb9ae6ce89c848f1417e92f0
WhalesAreNice <an1994xp@gmail.com>
2017-10-10 15:11:13 -0400

    added new code and new assets

--- first_game/game.js
+++ first_game/game.js
@@ -7,10 +7,11 @@ var salve;
 const SPEED = 4;
 const GRAVITY = 1;
 const JUMP_SPEED = SPEED*4;
-const NUM_CLOUDS = 5;
+const NUM_CLOUDS = 10;
 const NUM_BUSHES = 8;
 const NUM_WALLS = 3;
-const NUM_SALVE = 40;
+const NUM_SALVE = 1;
+const CAMERA_SPEED = 5;
 
 
 function setup() {
@@ -29,19 +30,36 @@ function setup() {
     
     //platform setup
     platform = createSprite(width/2, height - 10, width*2, 20);
-    platform.debug = true;
+    
+//    platform = loadImage("assets/platform/platform3.png");
+    
+//    const plat_img = loadImage("assets/platform/platform3.png");
+//    plat.addImage(plat_img);
+//    platform.add(plat);
+//    
+//    platform.debug = true;
     
     
     walls = new Group();
     for (let i = 0; i < NUM_WALLS; i++){
         const wall = createSprite(
-        random(32,width),
-        height*4/5,
+        random(width,width*1.5),
+        height*5/6,
         40,
-        height/3
+        height/3.5
         );
+        
+        const wall_imgs = ["assets/walls/wall1.png"];
+        
+        for(let i = 0; i < NUM_WALLS; i++) {
+            const wall_img = loadImage(wall_imgs[floor(random(0, wall_imgs.length))]);
+            wall.addImage(wall_img);
+        }
+        
         walls.add(wall);
     }
+    walls.debug = true;
+    
     
     
     
@@ -50,24 +68,39 @@ function setup() {
     for (let i = 0; i < NUM_CLOUDS; i++) {
         const cloud = createSprite(
             random(width, width*2),
-            random(height/2, 0),
+            random(height/3, 0),
             random(50,100),
             random(30,50)
         );
+        
+        const cloud_imgs = ["assets/clouds/cloud1.png" , "assets/clouds/cloud2.png" , "assets/clouds/cloud3.png" , "assets/clouds/cloud4.png" , "assets/clouds/cloud5.png"];
+        
+        for(let i = 0; i < NUM_CLOUDS; i++) {
+            const cloud_img = loadImage(cloud_imgs[floor(random(0, cloud_imgs.length))]);
+            cloud.addImage(cloud_img);
+        }
+        
         cloud.velocity.x = -random(5,10);
         clouds.add(cloud);
     }
     
-    beam = createSprite(
-        0,
-        0,
-        100,
-        height*2
-    );
+    beam = createSprite(0,0,100,height*2);
+    
     
     salve = new Group();
     for (let i = 0; i < NUM_SALVE; i++){
+        const life = createSprite (
+        random(0, width),
+        random(height/2, height),
+        30,
+        20
+        );
         
+        const salve_img = loadImage("assets/rewards/salve.png");
+        life.addImage(salve_img);
+        
+        salve.add(life);
+        salve.debug = true;
     }
     
     
@@ -80,7 +113,7 @@ function draw() {
         const cloud = clouds[i];
         if (cloud.position.x+cloud.width/2 < 0){
             cloud.position.x = random(width, width*2);
-            cloud.position.y = random(0, height/2);
+            cloud.position.y = random(0, height/3);
         }
     }
     
@@ -101,11 +134,15 @@ function draw() {
         character.velocity.y += GRAVITY;
     }
     
-    if (keyWentDown("x")){
+    if (keyWentDown("SPACE")){
         if (!character.isJumping){
             character.velocity.y -= JUMP_SPEED;
             character.isJumping = true;
         }
+    }
+    
+    if (keyWentDown("w")) {
+        character.position.x += 200;
     }
     
     
@@ -115,18 +152,29 @@ function draw() {
         character.lives--;
     }
     
+    for (let i = 0; i < salve.length; i++) {
+        const life = salve[i];
+        if (character.overlap(life)) {
+            character.lives += 40;
+            life.position.x += random(width*2, width*6);
+        } else {
+            wrap (life, random(width*2, width*6));
+        }
+    }
+    
+    
 //    wrapping platform
     wrap(platform, width);
-//    for(let i = 0: i < walls.length; i++) {
-//        const wall = walls[i];
-//        wrap(wall, random(width*2, width*4);)
-//    }
+    for(let i = 0; i < walls.length; i++) {
+        const wall = walls[i];
+        wrap(wall, random(width*1.5, width*3));
+    }
     
     
 //    camera and object constant movement
-    camera.position.x += 2;
-    beam.position.x += 2;
-    character.position.x += 2;
+    camera.position.x += CAMERA_SPEED;
+    beam.position.x += CAMERA_SPEED;
+    character.position.x += CAMERA_SPEED;
     
     drawSprites();
     drawSprites(walls);
